11cd8bb3d3411c6e51ca01f006f2b59de50cd357
dmaworsley <dmaworsley@gmail.com>
2020-11-20 11:55:58 +0100

    Helps if you update this as well

--- definitions.go
+++ definitions.go
@@ -7,11 +7,11 @@ package godb
 var readUser string
 var readPass string
 var readHost string
-var readPort string
+var readPort int
 var readDB string
 
 var writeUser string
 var writePass string
 var writeHost string
-var writePort string
+var writePort int
 var writeDB string
